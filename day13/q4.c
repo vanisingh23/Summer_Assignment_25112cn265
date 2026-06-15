948a21f0161fb63de434682500445256d318c58b
vanisingh23 <vanisingh0223@gmail.com>
2026-06-15 12:57:58 +0530

    day13 uploaded

---
+++ day13/q4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], n, i;
+    int evenCount = 0, oddCount = 0;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        if(arr[i] % 2 == 0)
+        {
+            evenCount++;
+        }
+        else
+        {
+            oddCount++;
+        }
+    }
+
+    printf("Number of even elements = %d\n", evenCount);
+    printf("Number of odd elements = %d\n", oddCount);
+
+    return 0;
+}
