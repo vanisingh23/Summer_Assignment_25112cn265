8d1b23fac43e45f9ecc05ee1f7c1b465061cc4d7
vanisingh23 <vanisingh0223@gmail.com>
2026-06-21 13:07:53 +0530

    day21 uploaded

---
+++ day21/q3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int vowels = 0, consonants = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        char ch = str[i];
+
+        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+            if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' ||
+                ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                vowels++;
+            else
+                consonants++;
+        }
+    }
+
+    printf("Vowels = %d\n", vowels);
+    printf("Consonants = %d\n", consonants);
+
+    return 0;
+}
